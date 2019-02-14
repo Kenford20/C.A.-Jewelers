cb7b00ab6f8fa6ec1f81e7a00a659f41fdb1958a
Kenford <kzhou0066@gmail.com>
2019-02-14 01:37:13 -0500

    updated products model to have more properties

--- models/product.js
+++ models/product.js
@@ -2,10 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-    imagePath:{type:String, required:true},
-    title:{type:String, required:true},
-    description:{type:String, required:true},
-    price:{type:Number, required:true},
+    imagePath:   { type:String, required:true },
+    title:       { type:String, required:true },
+    description: { type:String, required:true },
+    price:       { type:Number, required:true },
+    category:    { type:String, required:true },
+    subcategory: { type:String, required:true },
+    stoneShape:  { type:String, required:false },
+    metal:       { type:String, required:true }
 });
 
 module.exports = mongoose.model('Product', schema);
